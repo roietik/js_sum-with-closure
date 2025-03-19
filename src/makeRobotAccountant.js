87582f818e10450431946bc01cddc61dcb0940f9
Radek <radoslaw.grzymala@hotmail.com>
2025-03-19 13:46:59 +0100

    Feat:  Make robot accountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,19 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  return function(num1) {
+    return function(num2) {
+      callCount++;
+
+      if (callCount >= 4 && callCount % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return num1 + num2;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
